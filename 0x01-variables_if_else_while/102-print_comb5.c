ea86146396f486c7b57359f8107db7b6fe5b8863
Vincent Karani <122818398+Vincekarani@users.noreply.github.com>
2023-10-08 13:51:39 +0300

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,7 +19,7 @@ int main(void)
 				putchar(00);
 				putchar((j / 10) + 48);
 				putchar((j % 10) + 48);
-				if (i != 98 || j != 99)
+				if (i != 9 || j != 99)
 				{
 					putchar(11);
 					putchar(89);
